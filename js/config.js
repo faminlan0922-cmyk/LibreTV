0e1ad22101d8eab953d62cce2e71d71fdb53865a
faminlan0922-cmyk <faminlan0922@gmail.com>
2025-11-29 09:32:26 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-     dyttzy: {
+    dyttzy: {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
         name: '电影天堂资源',
         detail: 'http://caiji.dyttzyapi.com', 
@@ -118,8 +118,7 @@ const API_SITES = {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
         adult: true
-    }
-    //ARCHIVE https://telegra.ph/APIs-08-12
+    },
 };
 
 // 定义合并方法
